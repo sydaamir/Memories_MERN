dc42195502b181efb932523ff3720cf4b5e650df
Syed Aamir <syedaamirsan@gmail.com>
2020-11-05 21:15:17 +0530

    create post logic

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -16,6 +16,6 @@ export const createPost = (post) => async (dispatch) => {
         const { data } = await api.createPost(post);
         dispatch({ type: 'CREATE', payload: data });
     } catch (error) {
-        
+        console.log(error);
     }
 }
