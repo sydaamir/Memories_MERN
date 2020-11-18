abc0208412ca5af476b6f67e4a4d88165e946c5e
Syed Aamir <syedaamirsan@gmail.com>
2020-11-19 00:05:47 +0530

    fetching posts from mongodb and displaying it on ui

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-
 import Post from './Post/Post';
 import useStyles from './styles';
 
@@ -12,11 +12,17 @@ const Posts = () => {
     console.log('posts are here....',posts);
 
     return(
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6} >
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 export default Posts;
